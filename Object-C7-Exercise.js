576ed79a9d34ce414c8125f498a4c79dcd3e457f
Thanhdraw <dangquocthanh2812@gmail.com>
2023-07-12 15:31:40 +0700

    Exercise-obj

---
+++ Object-C7-Exercise.js
@@ -0,0 +1,159 @@
+// 1. viết 1 function kiểm tra có phải là object hay khong
+
+function isObject(value){
+    if(typeof value === "object" && !Array.isArray(value) && value !==null){
+        return true;
+    }
+    return false;
+}
+
+// console.log(isObject());
+
+// bài 2: Viết 1 function trả về 1 mảng key và value
+// Cach 1:
+function objectToArray(obj){
+    if(!isObject(obj)) return;
+    // Cách 1:
+        // return Object.entries(obj);
+    // Cách 2:
+        // const value = Object.keys(obj).map((key)=>[key,obj[key]]);
+    // return value;
+    // Cách 3:
+        let result = [];
+        for(let key in obj){
+            // hasOwnPropety(key) -> Nếu obj chưa key thì trả về true , ng lai trả về false
+            if(obj.hasOwnProperty(key)){
+                result.push([key,obj[key]]);
+            }
+        }
+    return result;
+
+}
+console.log(objectToArray({a:1, b:2}));
+
+
+// Bài 3: withThout
+
+
+
+
+// Bài 4: 
+
+
+// Bài tập: Quản lý danh sách sinh viên
+
+// Yêu cầu:
+
+// Tạo một đối tượng Student với các thuộc tính sau:
+
+// Tên (name): chuỗi
+// Tuổi (age): số nguyên
+// Điểm số (grade): số thực
+// Tạo một phương thức cho đối tượng Student để hiển thị thông tin của sinh viên:
+
+// Phương thức này sẽ in ra màn hình thông tin của sinh viên bao gồm tên, tuổi và điểm số.
+// Tạo một đối tượng Classroom để quản lý danh sách sinh viên:
+
+// Classroom sẽ có một thuộc tính là students (danh sách sinh viên), ban đầu là một mảng rỗng.
+// Classroom sẽ có phương thức để thêm một sinh viên mới vào danh sách (thêm vào mảng students).
+// Classroom sẽ có phương thức để hiển thị danh sách sinh viên.
+
+
+// Định nghĩa đối tượng Student
+function Student( name,age,grade){
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+}
+// Định nghĩa phương thức hiển thị thông tin sinh viên
+
+
+// 
+
+
+Student.prototype.displayInfo = function(){
+    console.log("-Name: " + this.name);
+    console.log("-Age: " + this.age);
+    console.log("-Grade: " + this.grade);
+
+}
+
+// Hiển thị thông tin sinh viên
+// user1.displayInfo();
+// cau1: thay doi thong tin sinh vien:
+Student.prototype.changeName = function(newName){
+    this.name = newName;
+}
+var user1 = new Student ("Thành",20,9.0);
+console.log("**Thông tin sinh viên trước khi thay đổi**");
+user1.displayInfo();
+//Thay đổi thông tin sinh viên
+user1.changeName("quocthanh");
+console.log("**Thông tin sinh viên sau khi thay đổi**");
+user1.displayInfo();
+
+// Thay doi tuoi sinh vien
+
+Student.prototype.changeAge = function(newAge){
+    this.age = newAge;
+}
+Student.prototype.changeGrade = function(newGrade) {
+    this.grade = newGrade;
+}
+user1.changeAge(18)
+console.log("**Thông tin sinh viên sau khi thay đổi**");
+user1.displayInfo();
+
+// Tạo class-room
+function Classroom() {
+    this.students = [];
+  }
+  
+  // Phương thức thêm sinh viên vào danh sách
+  Classroom.prototype.addStudent = function(student) {
+    this.students.push(student);
+  }
+  
+  // Phương thức xoá sinh viên khỏi danh sách
+  Classroom.prototype.removeStudent = function(student) {
+    var index = this.students.indexOf(student);
+    if (index > -1) {
+      this.students.splice(index, 1);
+      console.log("Đã xoá sinh viên khỏi danh sách.");
+    } else {
+      console.log("Sinh viên không tồn tại trong danh sách.");
+    }
+  }
+  Classroom.prototype.updateStudentInfo = function(student, newName, newAge, newGrade) {
+    var index = this.students.indexOf(student);
+    if (index > -1) {
+      this.students[index].changeName(newName);
+      this.students[index].changeAge(newAge);
+      this.students[index].changeGrade(newGrade);
+      console.log("Đã cập nhật thông tin của sinh viên.");
+    } else {
+      console.log("Sinh viên không tồn tại trong danh sách.");
+    }
+  }
+
+  // Tạo một đối tượng Classroom mới
+var classroom = new Classroom();
+
+// Tạo và thêm một số sinh viên vào danh sách
+var student1 = new Student("John", 20, 8.5);
+var student2 = new Student("Jane", 19, 7.8);
+classroom.addStudent(student1);
+classroom.addStudent(student2);
+classroom.updateStudentInfo(student2, "Jane Smith", 20, 9.2);
+classroom.students.forEach(function(student) {
+    student.displayInfo();
+    console.log("----------------------");
+  });
+  classroom.removeStudent(student1);
+classroom.updateStudentInfo(student2, "Jane Smith", 30, 9.2);
+// Hiển thị danh sách sinh viên sau khi cập nhật
+console.log("Danh sách sinh viên sau khi cập nhật:");
+classroom.students.forEach(function(student) {
+  student.displayInfo();
+  console.log("----------------------");
+});
--- Object-C7.js
+++ Object-C7.js
@@ -13,7 +13,7 @@ console.log(student);
 // For in
 for(key in student){
 
-    console.log(`${key}: ${student[key]}`);
+        console.log(`${key}: ${student[key]}`);
     
 }
 // Object.keys()
@@ -37,4 +37,86 @@ console.log(enTries);
     // changeCar.nameCar = "Audi";
     // console.log(changeCar);
 
+// Cach sao chep 1 Object
+// const student = {
+//     id: 1911062578,
+//     Name: "Quoc Thanh",
+//     Major: "IT",
+//     DTime : "2019 -2023",
+// }
+const newStudent = {...student};
+console.log(newStudent);
+console.log(student);
+// {id: 1911062578, Name: 'Quoc Thanh', Major: 'IT', DTime: '2019 -2023', hello: ƒ}
 
+const newUser2 = Object.assign({},student);
+console.log(newUser2);
+
+
+const user = {
+    idStudent: "123",
+    nameStudent: "thanhdang",
+    school: {
+        nameSchool: "Hutech",
+        room: {
+            nameRoom: "IT"
+        },
+    },
+};
+console.log(user);
+// console.log("///////ban sao thu 2 cua user")
+// const user2 = Object.assign({},user);
+// user2.nameStudent = "quocthanh";
+// console.log(user2);
+// console.log("///////ban sao thu 3 cua user")
+// const user3 = Object.assign({},user);
+// user3.school = "SPKT";
+// console.log(user3);
+console.log("///////user sao khui dung ...");
+const newUser = {...user};
+newUser.nameStudent = "quocthanh";
+console.log(newUser);
+
+// Object.assign()
+console.log("///////user3 sao khi dung assign");
+const newUser3 = JSON.parse(JSON.stringify(user));
+newUser3.nameStudent="minhthao(trapgirl)"
+newUser3.school.nameSchool = "SPKT";
+console.log(newUser3);
+
+
+
+
+
+// This keyword
+const student2 = {
+    id: 1911062578,
+    Name: "Quoc Thanh",
+    Major: "IT",
+    DTime : "2019 -2023",
+    hi: function(){
+        console.log(`my name is ${this.Name}`);
+    },
+    fullName: {
+        nameCon: "Dang Quoc Thanh"
+    },
+};
+student2.hi();
+// console.log(student2);
+console.log(student2.fullName?.nameCon);
+
+
+
+
+
+
+// Destructuring object
+console.log("////Destructuring object");
+function whatYourinfo({name,age,school}){
+    console.log(name,age,school);
+}
+  whatYourinfo({
+    age: 23,
+    name : "thanhDev",
+    school: "Hutech",
+  });
