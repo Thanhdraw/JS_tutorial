858dce0b980e917cb752b8e996d29661d7499cf1
Thanhdraw <dangquocthanh2812@gmail.com>
2023-07-31 19:36:20 +0700

    thanhne

---
+++ Exercise/Tab/app.js
@@ -0,0 +1,21 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContents = document.querySelectorAll(".tab-content");
+[...tabItems].forEach((item) => item.addEventListener("click", handleClick));
+
+function handleClick(event) {
+  // console.log(event.target);
+  // Xoá hết tất cả các class active ở tab-item
+  [...tabItems].forEach((item) => item.classList.remove("active"));
+  // sau đó add class active vào tab hiện tại
+  event.target.classList.add("active");
+  
+  const tabNumber = event.target.dataset.tab;
+
+  [...tabContents].forEach((item) => {
+    item.classList.remove("active");
+    if(item.getAttribute("data-tab")===tabNumber){
+        item.classList.add("active");
+    }
+  });
+  // [...tabContents][tabNumber - 1].classList.add("active");
+}
